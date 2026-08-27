637671469d4b4b4631d66cfe98755e44a97f2d5c
MAHI SINHA <sinhamahi2005@gmail.com>
2026-08-27 19:44:31 +0530

    Time: 3 ms (2.47%), Space: 16.6 MB (6.59%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -8,40 +8,60 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+ ListNode* reverseLL(ListNode* head){
+    if(head==NULL||head->next==NULL)return head;
+    ListNode*temp=head;
+    ListNode* prevNode=NULL;
+    while(temp!=NULL){
+        ListNode* nextNode;
+        nextNode=temp->next;
+        temp->next=prevNode;
+        prevNode=temp;
+        temp=nextNode;
+    }
+    return prevNode;
+ }
+
+ ListNode* findKth(ListNode* head, int k){
+    int count=0;
+    ListNode* temp=head;
+    while(temp!=NULL){
+        count++;
+        temp=temp->next;
+    }
+    if(count<k)return NULL;
+    temp=head;
+    for(int i=1;i<k;i++){
+        temp=temp->next;
+    }
+    return temp;
+
+ }
 class Solution {
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
-        if (head == NULL || k == 1)
-            return head;
-
-        ListNode* temp = head;
-
-        // Check whether k nodes are available
-        int count = 0;
-        while (temp != NULL && count < k) {
-            temp = temp->next;
-            count++;
+        ListNode* temp=head;
+        ListNode* prevLast=NULL;
+        while(temp!=NULL){
+        ListNode* kth=findKth(temp,k);
+        if(kth==NULL){
+            if(prevLast)prevLast->next=temp;
+            break;
         }
-
-        // Less than k nodes -> don't reverse
-        if (count <k)
-            return head;
-
-        // Reverse first k nodes
-        ListNode* prev = NULL;
-        ListNode* curr = head;
-
-        for (int i = 0; i < k; i++) {
-            ListNode* nextNode = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = nextNode;
+        ListNode* nextNode=kth->next;
+        kth->next=NULL;
+        reverseLL(temp);
+        if(temp==head){
+            head=kth;
         }
+        else{
+        prevLast->next=kth;
+        }
+        prevLast=temp;
+        temp=nextNode;
 
-        // head is now the last node of reversed group
-        head->next = reverseKGroup(curr, k);
-
-        // prev i
-        return prev;
+        }
+        return head;
     }
 };
